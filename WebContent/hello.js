3c6ded8f604111782548309963bae5e4c311e5a0
bit-user <bit-user@bit>
2016-10-26 15:52:55 +0900

    js

--- WebContent/hello.js
+++ WebContent/hello.js
@@ -1,1 +1,6 @@
-document.write( "Hello World" ); 
+document.write("<h1>Hello Javascript</h1>");
+console.log("Hello World");
+console.info("Hello World");
+console.debug("Hello World");
+console.error("Hello World");
+console.warn("Hello World");
